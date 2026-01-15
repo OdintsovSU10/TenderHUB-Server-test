b1c6689c531b7f46acd754152761cf29428f42a8
OdintsovSU10 <odintsov.su10@gmail.com>
2026-01-15 18:03:21 +0300

    Добавить barrel exports в src/hooks и src/utils
    - Создать src/hooks/index.ts для useDeadlineCheck
    - Создать src/utils/index.ts (главный barrel export)
    - Реэкспорт всех поддиректорий (boqItems, debug, excel, matching, rounding, versionTransfer)
    - Реэкспорт всех корневых утилит (calculateGrandTotal, copyBoqItems, и др.)
    - Упрощает импорты утилит в приложении

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+// Subdirectories
+export * from './boqItems';
+export * from './debug';
+export * from './excel';
+export * from './matching';
+export * from './rounding';
+export * from './versionTransfer';
+
+// Root utilities
+export * from './calculateGrandTotal';
+export * from './copyBoqItems';
+export * from './deadlineCheck';
+export * from './initializeTestMarkup';
+export * from './insertTemplateItems';
+export * from './markupCalculator';
+export * from './numberFormat';
+export * from './parseExcelForVersion';
+export * from './pluralize';
+export * from './versionColor';
